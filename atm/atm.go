2dc9f9afc2be9db4b95cc3a49f9d21aaf1fe684d
Ángel Siendones Sillero <culoextremo@gmail.com>
2024-01-03 13:09:25 +0100

    feat: withdraw two banknotfeat: withdraw 600 euros

--- atm/atm.go
+++ atm/atm.go
@@ -9,12 +9,19 @@ func NewAtm() *Atm {
 
 func Withdraw(amount int) []Note {
 	res := make([]Note, 0)
-	res = append(res, Note{500})
-	if amount == 1000 {
-		res = append(res, Note{500})
-	}
-	if amount == 600 {
-		res = append(res, Note{100})
+	remainder := amount
+	for remainder > 0 {
+		if remainder >= 500 {
+			res = append(res, Note{500})
+			remainder -= 500
+		} else if remainder >= 100 {
+			res = append(res, Note{100})
+			remainder -= 100
+		}
 	}
 	return res
+}
+
+func AccumulateBill(bunch *[]Note) {
+
 }
